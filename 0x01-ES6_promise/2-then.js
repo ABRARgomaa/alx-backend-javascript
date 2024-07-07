2b2cbe49c5c4e43f6323e4525f020f1df2ed0835
ABRARgomaa <abrargomaa111@gmail.com>
2024-07-07 08:48:01 +0300

    up

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,8 +2,8 @@ export default function handleResponseFromAPI(promise) {
   return promise.then(() => {
     console.log({ status: 200, body: 'success' });
   }).catch(() => {
-    console.error();
-    return new Error();
+    console.error('Error occurred');
+    return new Error('Error occurred');
   }).finally(() => {
     console.log('Got a response from the API');
   });
