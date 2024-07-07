91ac58b1cb91dc6aaab67d96a219e74b5f245a91
ABRARgomaa <abrargomaa111@gmail.com>
2024-07-07 23:53:38 +0300

    up

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,18 +4,8 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const sign = signUpUser(firstName, lastName);
   const upload = uploadPhoto(fileName);
-  return Promise
-    .all([sign, upload])
-    .then((result) => {
-      result.map((result) => ({
-        status: 'fulfilled',
-        value: result,
-      }));
-    })
-    .catch((error) => {
-      error.map((error) => ({
-        status: 'rejected',
-        value: error,
-      }));
-    });
+  return Promise.allSettled([sign, upload]).then((result) => result.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : result.reason,
+  })));
 }
