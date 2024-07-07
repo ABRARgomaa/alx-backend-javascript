f15903b4a71d0f5f09d3ed6103a1e75621b20300
ABRARgomaa <abrargomaa111@gmail.com>
2024-07-07 06:06:04 +0300

    up

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,7 +1,7 @@
-export default function getResponseFromAPI(){
-    const promise = new Promise((res, rej) => {
-        res('success');
-        rej(Error('fail'));
-    });
-    return promise;
-};
+export default function getResponseFromAPI() {
+  const promise = new Promise((res, rej) => {
+    res('success');
+    rej(Error('fail'));
+  });
+  return promise;
+}
