a45cf07e06f933716aa3fd8fc4f72c4bc3d3b19f
ABRARgomaa <abrargomaa111@gmail.com>
2024-07-08 03:53:25 +0300

    up

--- 0x02-ES6_classes/.eslintrc.js
+++ 0x02-ES6_classes/.eslintrc.js
@@ -18,8 +18,6 @@ module.exports = {
     },
     plugins: ['jest'],
     rules: {
-      'max-classes-per-file': 'off',
-      'no-underscore-dangle': 'off',
       'no-console': 'off',
       'no-shadow': 'off',
       'no-restricted-syntax': [
@@ -34,4 +32,4 @@ module.exports = {
         excludedFiles: 'babel.config.js',
       }
     ]
-  };
+};
