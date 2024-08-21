bbfa0b7ebbeec10b2601089bc03bb2faa5444183
ABRARgomaa <abrargomaa111@gmail.com>
2024-08-21 19:26:50 +0300

    up

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,2 +1,9 @@
-var calculateNumber = require("./0-calcul");
-var chai = require("chai");
+var calculateNumber = require("./0-calcul")
+const assert = require('assert');
+
+describe("test suit", function() {
+    it("test the calculateNumber function", function() {
+        const result = calculateNumber(2, 3);
+        assert.strictEqual(result, 5);
+    });
+});
