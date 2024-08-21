fb2791132288e2718cef905a726a1215d8ff8845
ABRARgomaa <abrargomaa111@gmail.com>
2024-08-21 03:01:28 +0300

    up

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,6 +1,11 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ */
 function displayMessage(string)
 {
   console.log(string);
-}
+};
 
 module.exports = displayMessage;
