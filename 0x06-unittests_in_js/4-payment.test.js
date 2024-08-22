377b72230bec18e152d7f9e0dcb42e4c22ed8a88
ABRARgomaa <abrargomaa111@gmail.com>
2024-08-22 17:32:42 +0300

    up

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -5,16 +5,16 @@ const sendPaymentRequestToApi = require('./4-payment');
 
 describe('sendPaymentRequestToApi', () => {
   it('sendPaymentRequestToApi calls console.log with the right arguments', () => {
-    const consolespy = sinon.spy(console);
-    const calculateNumberStub = sinon.stub(Utils, 'calculateNumber');
+    const bigBrother = sinon.spy(console);
+    const dummy = sinon.stub(Utils, 'calculateNumber');
 
-    calculateNumberStub.returns(10);
+    dummy.returns(10);
     sendPaymentRequestToApi(100, 20);
-    expect(calculateNumberStub.calledWith('SUM', 100, 20)).to.be.true;
-    expect(calculateNumberStub.callCount).to.be.equal(1);
-    expect(consolespy.log.calledWith('The total is: 10')).to.be.true;
-    expect(consolespy.log.callCount).to.be.equal(1);
-    calculateNumberStub.restore();
-    consolespy.log.restore();
+    expect(dummy.calledWith('SUM', 100, 20)).to.be.true;
+    expect(dummy.callCount).to.be.equal(1);
+    expect(bigBrother.log.calledWith('The total is: 10')).to.be.true;
+    expect(bigBrother.log.callCount).to.be.equal(1);
+    dummy.restore();
+    bigBrother.log.restore();
   });
 });
