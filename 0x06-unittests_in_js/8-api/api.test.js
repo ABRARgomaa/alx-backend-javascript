104d3b39db47a8b869bde1a4c3025c123d301b73
ABRARgomaa <abrargomaa111@gmail.com>
2024-08-23 01:26:13 +0300

    up

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,27 +1,14 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const expect = chai.expect;
-const app = require('./index');
+const request = require('request');
+const { expect } = require('chai');
 
-chai.use(chaiHttp);
+describe('API integration test', () => {
+  const API_URL = 'http://localhost:7865';
 
-describe('Index Page Tests', function() {
-  it('should return status code 200 for the index page', function(done) {
-    chai.request(app)
-    .get('/')
-    .end((err, res) => {
-        if (err) return done(err);
-        expect(res).to.have.status(200);
-        done();
-    });
-  });
-  it('should return the correct result for the index page', function(done) {
-    chai.request(app)
-    .get('/')
-    .end((err, res) => {
-        if (err) return done(err);
-        expect(res.text).to.equal('Hello World!');
-        done();
+  it('GET / returns correct response', (done) => {
+    request.get(`${API_URL}/`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
+      done();
     });
   });
 });
