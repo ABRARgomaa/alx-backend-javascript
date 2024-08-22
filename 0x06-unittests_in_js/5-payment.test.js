ffbbd3c95d7a556ba6466308ef24bc12496aeb6c
ABRARgomaa <abrargomaa111@gmail.com>
2024-08-22 18:45:04 +0300

    up

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -4,19 +4,22 @@ const chai = require("chai");
 const expect = chai.expect;
 
 describe("test case for hooks", function() {
+    let consoleLog;
     beforeEach(function() {
+        consoleLog = sinon.spy(console, 'log');
+    })
+    afterEach(function(){
         sinon.restore();
-    });
+    })
     it("sendPaymentRequestToAPI with 100, and 20", function() {
-        const consoleLog = sinon.spy(console);
+        
         sendPaymentRequestToApi(100, 20);
-        expect(consoleLog.log.calledWith('The total is: 120')).to.be.true;
-        expect(consoleLog.log.callCount).to.be.equal(1);
+        expect(consoleLog.calledWith('The total is: 120')).to.be.true;
+        expect(consoleLog.callCount).to.be.equal(1);
     });
     it("sendPaymentRequestToAPI with 10, and 10", function() {
-        const consoleLog = sinon.spy(console);
         sendPaymentRequestToApi(10, 10);
-        expect(consoleLog.log.calledWith('The total is: 20')).to.be.true;
-        expect(consoleLog.log.callCount).to.be.equal(1);
+        expect(consoleLog.calledWith('The total is: 20')).to.be.true;
+        expect(consoleLog.callCount).to.be.equal(1);
     });
 });
